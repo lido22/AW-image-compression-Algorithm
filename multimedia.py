589732c04a5afdff36f55ac23222ea93088e1172
lido22 <ahmedcs2022@yahoo.com>
2020-06-01 09:10:04 +0200

    Final version

--- multimedia.py
+++ multimedia.py
@@ -1,12 +1,10 @@
+"""
+After Running the program if you want to decode 
+run decode() in terminal
+"""
 import cv2
 import numpy as np
 import timeit
-
-
-
-#Your statements here
-
-
 
 """
 input: imageName
@@ -28,35 +26,39 @@ def createDict(flat, delta):
     l = len(flat)-1
     for i in flat:
         if abs(int(prev)-int(i)) > delta or counter==255:
-            arr.append((counter, prev))
+            arr.append(counter)
+            arr.append(prev)
             prev = i
             counter = 0
         counter += 1
     if(l+1 != len(arr)):
-        arr.append((counter, prev))
+        arr.append(counter)
+        arr.append(prev)
     return arr
-            
 
 imageName = input("Enter image Name: ")
 delta = int(input("Enter Delta: "))
 start = timeit.default_timer()
 flat, dimensions = readImage(imageName)
 arr = createDict(flat, delta)
-dt = np.dtype('uint8, uint8')
+
+dt = np.dtype('uint8')
 code = np.array(arr, dt)
 stop = timeit.default_timer()
 print('Encoding Time: ', stop - start)  
-np.savez_compressed("code", code) 
-    
+np.savez_compressed("coded "+imageName, code) 
+
 def decode():
     file = np.load("code.npz")
     start = timeit.default_timer()
     code = file.f.arr_0
     decod = []
-    for limit, value in code:
-        for i in range(limit):
-            decod.append(value)
+    count = 0
+    for j in range(int(len(code)/2)):
+        for i in range(code[count]):
+            decod.append(code[count+1])
+        count += 2
     finalArr = np.reshape(np.array(decod, dtype=np.uint8), dimensions)
     stop = timeit.default_timer()
     print('Decoding Time: ', stop - start) 
-    cv2.imwrite('decoded'+'lens.png', finalArr)
+    cv2.imwrite('decoded '+imageName, finalArr)
